58f754a1cc6c100fb5bd36150a818d8c837bbd4d
Alejomv <amartinezvelez@gmail.com>
2023-09-17 00:17:07 -0500

    Ejemplo_Regla_3

--- vista1.js
+++ vista1.js
@@ -1,70 +1,75 @@
-import React from "react";
-import {
-  Text,
-  View,
-  Image,
-  StyleSheet,
-  Button,
-  TouchableOpacity,
-  Alert,
-} from "react-native";
+import React, { useState } from 'react';
+import { View, Text, TextInput, Button, StyleSheet, ScrollView } from 'react-native';
 
-function Vista1() {
+const Calculadora = () => {
+  const [valor1, setValor1] = useState('');
+  const [valor2, setValor2] = useState('');
+  const [valorX, setValorX] = useState('');
+  const [resultadoSimple, setResultadoSimple] = useState(null);
+
+  const [a, setA] = useState('');
+  const [b, setB] = useState('');
+  const [c, setC] = useState('');
+  const [d, setD] = useState('');
+  const [e, setE] = useState('');
+  const [resultadoCompuesto, setResultadoCompuesto] = useState(null);
+
+  const calcularSimple = () => {
+    const res = (parseFloat(valorX) * parseFloat(valor2)) / parseFloat(valor1);
+    setResultadoSimple(res);
+  };
+
+  const calcularCompuesto = () => {
+    const res = (parseFloat(d) * parseFloat(e) * parseFloat(c)) / (parseFloat(a) * parseFloat(b));
+    setResultadoCompuesto(res);
+  };
+
   return (
-    <View>
-      <Image
-        style={{ height: 200, width: 200 }}
-        source={require("./assets/splash.png")}
-      />
+    <ScrollView style={styles.container}>
+      <View style={{ marginTop: 40 }}></View>
 
-      <Image
-        style={{ height: 200, width: 200 }}
-        source={{
-          uri: "https://drive.google.com/uc?export=view&id=136hBemaBg3HcyvXh0XcsVXujbsGhJ2JC",
-        }}
-      />
+      <Text style={styles.title}>Regla de Tres Simple</Text>
+      <Text style={styles.description}>Si 10 manzanas cuestan $20, ¿cuánto cuestan 5 manzanas?</Text>
+      <TextInput style={styles.input} placeholder="Valor 1 (Ejemplo: 10 manzanas)" keyboardType="numeric" onChangeText={text => setValor1(text)} />
+      <TextInput style={styles.input} placeholder="Valor 2 (Ejemplo: $20)" keyboardType="numeric" onChangeText={text => setValor2(text)} />
+      <TextInput style={styles.input} placeholder="Valor X (Ejemplo: 5 manzanas)" keyboardType="numeric" onChangeText={text => setValorX(text)} />
+      <Button title="Calcular Simple" onPress={calcularSimple} />
+      <Text>Resultado Simple: {resultadoSimple !== null ? resultadoSimple : 'N/A'}</Text>
+      
+      <Text style={styles.title}>Regla de Tres Compuesta</Text>
+      <Text style={styles.description}>Hemos ido a la fuente del pueblo para recoger agua. Sabemos que 5 botellas de agua, de 2 litros cada una, pesan 10 kilos. ¿Cuánto pesan 2 botellas de 3 litros cada una?</Text>
+      <TextInput style={styles.input} placeholder="Valor a (Ejemplo: 5 botellas)" keyboardType="numeric" onChangeText={text => setA(text)} />
+      <TextInput style={styles.input} placeholder="Valor b (Ejemplo: 2 litros)" keyboardType="numeric" onChangeText={text => setB(text)} />
+      <TextInput style={styles.input} placeholder="Valor c (Ejemplo: 10 kilos)" keyboardType="numeric" onChangeText={text => setC(text)} />
+      <TextInput style={styles.input} placeholder="Valor d (Ejemplo: 2 botellas)" keyboardType="numeric" onChangeText={text => setD(text)} />
+      <TextInput style={styles.input} placeholder="Valor e (Ejemplo: 3 litros)" keyboardType="numeric" onChangeText={text => setE(text)} />
+      <Button title="Calcular Compuesto" onPress={calcularCompuesto} />
+      <Text>Resultado Compuesto: {resultadoCompuesto !== null ? resultadoCompuesto : 'N/A'}</Text>
 
-      <Button
-        title="CLick Aqui"
-        color="red"
-        onPress={() => Alert.alert("HOla desde el Boton")}
-      />
-
-      <TouchableOpacity
-        style={estilos.boton}
-        onPress={() => console.log("HOla desde otro Boton")}
-      >
-        <Text style={estilos.Letra}> Boton </Text>
-      </TouchableOpacity>
-
-      <Text style={estilos.Letra}> Hola desde vista 1 </Text>
-      <Image
-        style={estilos.TamanoImg}
-        source={require("./assets/splash.png")}
-      />
-
-      <Text>Hola desde vista 1</Text>
-    </View>
+      <View style={{ marginBottom: 100 }}></View>
+    </ScrollView>
   );
-}
+};
 
-let estilos = StyleSheet.create({
-  TamanoImg: {
-    height: 200,
-    width: 200,
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
   },
-  Letra: {
-    fontSize: 30,
-    color: "Black",
-    fontWeight: "bold",
+  title: {
+    fontSize: 24,
+    marginBottom: 16,
   },
-  boton: {
-    height: 30,
-    backgroundColor: "red",
-    color: "black",
-    border: "1px solid black",
-    marginTop: 10,
+  description: {
+    fontSize: 16,
+    marginBottom: 16,
+  },
+  input: {
+    fontSize: 18,
+    borderWidth: 1,
+    padding: 8,
+    marginBottom: 16,
   },
 });
 
-export default Vista1;
+export default Calculadora;
