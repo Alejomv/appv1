4c2a227fed7ac31eed34db04e98d7ce9e0d6455b
Alejomv <amartinezvelez@gmail.com>
2023-09-16 23:25:02 -0500

    Mejora_Formato

--- App.js
+++ App.js
@@ -1,11 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
-import Vista1 from './vista1';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Image } from "react-native";
+import Vista1 from "./vista1";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      
       <Vista1 />
       <StatusBar style="auto" />
     </View>
@@ -15,8 +14,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
